b76fecc4fb8d4f9f2e8c3a5aadb0e4feeb421111
brevre <brendamakena09@gmail.com>
2023-11-07 21:39:25 +0300

    fin

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { format } from "date-fns";
-import ExampleComponent from "./ExampleComponent";
+import React from 'react';
+import ExampleComponent from './ExampleComponent';
+import TestComponent from './TestComponent'; // Import the TestComponent if it's not already imported
 
-// Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      <h1>Now</h1> {/* Update the contents of the h1 element */}
       <p className="App-intro">
-        In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
-        syntax. JSX lets us include JavaScript functions right along with the
-        HTML, and also allows us to add in components, which are separate,
+        In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax. JSX lets us include JavaScript
+        functions right along with the HTML, and also allows us to add in components, which are separate,
         self-contained chunks of JSX.
       </p>
+      <div className="example">
+        <img
+          alt="a deeply impressed man, perhaps thinking 'whoa'"
+          src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        />
+        <p>Whoa!</p>
+      </div>
       <ExampleComponent />
+      <TestComponent /> {/* Add the new TestComponent just below ExampleComponent */}
     </div>
   );
 }
